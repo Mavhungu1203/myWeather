656c173e879f790dc2f7156f21162e6cf47ef03f
Mavhungu1203 <mavhungu.monjelele@gmail.com>
2024-03-06 00:02:02 +0200

    Added functions for cities, date  and formsearchSubmit

--- src/index.js
+++ src/index.js
@@ -10,12 +10,49 @@ function updateWeather(response){
     let emojiElement = document.querySelector(".emoji");
     
     cityElement.innerHTML = response.data.city; 
-    timeElement.innerHTML = formDate(date);
+    timeElement.innerHTML = dateFormat(date);
     descriptionElement.innerHTML = response.data.condition.description; 
     humidityElement.innerHTML =`${response.data.temperature.humidity}%`;
     windElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
-    emojiElement.innerHTML = `<img src= "${response.data.condition.icon_url}" class = "emoji"`;
+    emojiElement.innerHTML = `<img src= "${response.data.condition.icon_url}" class ="emoji"/>`;
 
 }
 
+function dateFormat(date){
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    let day = days[date.getDay()];
+    if (minutes < 10 ) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
+}
+
+function searchCity(city){
+    let apiKey = "c490e973443bd6bo83eaffa975bat298";
+    let apiUrl  = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(updateWeather);
+}
+
+function formSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", formSearchSubmit);
+
+searchCity("Thohoyandou");
