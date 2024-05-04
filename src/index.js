f6c33b1a0b74a65a65da4a55cc71bb4c4460d2a7
Mavhungu1203 <mavhungu.monjelele@gmail.com>
2024-05-04 21:39:15 +0200

    added a function to format day

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ function updateWeather(response){
     windElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
     emojiElement.innerHTML = `<img src= "${response.data.condition.icon_url}" class ="emoji"/>`;
+    
     getForecast(response.data.city);
 }
 
@@ -51,6 +52,13 @@ function formSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 
 function getForecast(city) {
   let apiKey = "c490e973443bd6bo83eaffa975bat298";
@@ -59,17 +67,31 @@ function getForecast(city) {
 }
 function displayForecast(response){
     let forecastHtml = "";
-    response.data.daily.forEach(function(day,index)){
+    response.data.daily.forEach(function(day,index){
         if(index < 5){
             forecastHtml +=
             `
             <div class="weather-forecast-day">
-                <div class="weather-forecast-date">
-
-            `
+                <div class="weather-forecast-date">${formatDay(day.time)}</div>
+                
+                <img src="${day.condition.icon_url}" class ="weather-forecast-icon" />
+                <div class="weather-forecast-temperatures">
+                    <div class="weather-forecast-temperature">
+                       <strong>${Math.round(day.temperature.maximum)}º</strong>
+                    </div>
+                    <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}º</div>
+                    </div>
+                </div>    
+            `;
         }
-    }
+    
+    });
+
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+
 }
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", formSearchSubmit);
 
